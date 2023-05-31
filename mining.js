bd2fa21efc360fb414019669dadb30da5749400a
jake <jacob.ray.cs@gmail.com>
2023-05-30 23:39:55 -0400

    Disable Buttons

--- mining.js
+++ mining.js
@@ -3,9 +3,25 @@ var activeMining = false;
 var miningRate = 1000;
 var miningYield = 1;
 
-const mybutton = document.getElementById("commonSelect");
-mybutton.disabled = true
-mybutton.style.background='#000000';
+const mybutton1 = document.getElementById("uncommonSelect");
+mybutton1.disabled = true;
+mybutton1.style.background='#000000';
+
+const mybutton2 = document.getElementById("rareSelect");
+mybutton2.disabled = true
+mybutton2.style.background='#000000';
+
+const mybutton3 = document.getElementById("epicSelect");
+mybutton3.disabled = true
+mybutton3.style.background='#000000';
+
+const mybutton4 = document.getElementById("eliteSelect");
+mybutton4.disabled = true
+mybutton4.style.background='#000000';
+
+const mybutton5 = document.getElementById("legendarySelect");
+mybutton5.disabled = true
+mybutton5.style.background='#000000';
 
 function StartSkill(){
     animateBar();
